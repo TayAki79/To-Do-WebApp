75defad531bebdb8f46cdea1cc9e59a3bfffcefc
Akradeth Tayarat <a.tayarat@gmx.net>
2023-10-19 19:37:42 +0200

    import path to make it accessible on web server

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 const port = 3000;
 
 app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
